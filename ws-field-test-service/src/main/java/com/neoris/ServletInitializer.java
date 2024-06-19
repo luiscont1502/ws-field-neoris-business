38edd39a40204f4849491b8d7c8038abb9f9da38
lachicaiza6 <lachicaiza6@espe.edu.ec>
2024-06-18 20:47:24 -0500

    rename repository

--- ws-field-test-service/src/main/java/com/neoris/ServletInitializer.java
+++ ws-field-test-service/src/main/java/com/neoris/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WsFieldNeorisBusinessApplication.class);
+		return application.sources(WsFieldTestBusinessApplication.class);
 	}
 
 }
--- ws-field-test-service/src/main/java/com/neoris/WsFieldTestBusinessApplication.java
+++ ws-field-test-service/src/main/java/com/neoris/WsFieldTestBusinessApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WsFieldNeorisBusinessApplication {
+public class WsFieldTestBusinessApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WsFieldNeorisBusinessApplication.class, args);
+		SpringApplication.run(WsFieldTestBusinessApplication.class, args);
 	}
 
 }
